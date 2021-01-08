34bcfe16fef2f4a75371cb6aa9a54c43d70fdc37
Liran Bass <basslir@post.bgu.ac.il>
2021-01-08 17:01:25 +0200

    update base:header:footer:liran

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 ###### App setup
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
@@ -8,22 +7,40 @@ app.config.from_pyfile('settings.py')
 ###### Pages
 ## Homepage
 from pages.homepage.homepage import homepage
+
 app.register_blueprint(homepage)
 
 ## About
 from pages.about.about import about
+
 app.register_blueprint(about)
 
-## Catalog
-from pages.catalog.catalog import catalog
-app.register_blueprint(catalog)
+## cart
+from pages.cart.cart import cart
 
-## Page error handlers
-from pages.page_error_handlers.page_error_handlers import page_error_handlers
-app.register_blueprint(page_error_handlers)
+app.register_blueprint(cart)
+#
+# ## Page error handlers
+# from pages.page_error_handlers.page_error_handlers import page_error_handlers
+# app.register_blueprint(page_error_handlers)
 
 
 ###### Components
 ## Main menu
 from components.main_menu.main_menu import main_menu
+
 app.register_blueprint(main_menu)
+
+from components.header.header import header
+
+app.register_blueprint(header)
+
+
+from components.footer.footer import footer
+
+
+app.register_blueprint(footer)
+
+
+if __name__ == '__main__':
+    app.run()
